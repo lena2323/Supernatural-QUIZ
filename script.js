15d2c6a553bc3fe04a0fd332357d48ec3b675e09
Lena2323 <lena.jovicic23@gmail.com>
2022-06-10 03:27:01 +0200

    It's slowly starting to work wow

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-/*const wrongButton = document.getElementById('wrongButton');
 
 
-wrongButton.addEventListener('click',() =>{
-    wrongButton.classList.add("wrong");
 
-});*/
+    let correctAnswerButton = document.getElementById('correctButton');
+    let wrongAnswerButton1 = document.getElementById('wrongButton1');
+    let wrongAnswerButton2 = document.getElementById('wrongButton2');
+    let wrongAnswerButton3 = document.getElementById('wrongButton3');
+
+
+    const disableButtonForRight = () => {
+        wrongAnswerButton1.disabled = true;
+        wrongAnswerButton2.disabled = true;
+        wrongAnswerButton3.disabled = true;
+      };
+
+    const disableButtonForWrong1 = () =>{
+        wrongAnswerButton2.disabled = true;
+        wrongAnswerButton3.disabled = true;
+        correctAnswerButton.disabled = true;
+
+    }
+
+    const disableButtonForWrong2 = () =>{
+        wrongAnswerButton1.disabled = true;
+        wrongAnswerButton3.disabled = true;
+        correctAnswerButton.disabled = true;
+    }
+
+    const disableButtonForWrong3= () =>{
+        wrongAnswerButton1.disabled = true;
+        wrongAnswerButton2.disabled = true;
+        correctAnswerButton.disabled = true;
+    }
+
+    correctAnswerButton.addEventListener('click', disableButtonForRight);
+    
+
+    wrongAnswerButton1.addEventListener('click', disableButtonForWrong1);
+    
+    wrongAnswerButton2.addEventListener('click', disableButtonForWrong2);
+
+    wrongAnswerButton3.addEventListener('click', disableButtonForWrong3);
+

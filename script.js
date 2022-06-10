10c0a38f50376c639bcb6390737d5df7cc0272e0
Lena2323 <lena.jovicic23@gmail.com>
2022-06-10 21:01:34 +0200

    It kinda works... kinda.

--- script.js
+++ script.js
@@ -40,3 +40,75 @@
     wrongAnswerButton2.addEventListener('click', disableButtonForWrong2);
 
     wrongAnswerButton3.addEventListener('click', disableButtonForWrong3);
+
+
+
+    var questions = [`
+    <p>Who is this?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >Bobby Singer</button>
+        <button class="allButtons wrong" id="wrongButton1">Jo Harvelle</button>
+        <button class="allButtons wrong" id="wrongButton2" >Castiel</button>
+        <button class="allButtons wrong" id="wrongButton3">Death</button>
+    </div>`,
+    `<p>Who is this?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >Dean Winchester</button>
+        <button class="allButtons wrong" id="wrongButton1">Sam Harvelle</button>
+        <button class="allButtons wrong" id="wrongButton2" >Crowley</button>
+        <button class="allButtons wrong" id="wrongButton3">War</button>
+    </div>`,
+    `<p>Who is this?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >Dean JOOJO</button>
+        <button class="allButtons wrong" id="wrongButton1">Sam Winchester</button>
+        <button class="allButtons wrong" id="wrongButton2" >Ruby</button>
+        <button class="allButtons wrong" id="wrongButton3">Azazel</button>
+    </div>`,
+    `<p>How do you kill a vampire?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >You look at him funny</button>
+        <button class="allButtons wrong" id="wrongButton1">You cut its head off</button>
+        <button class="allButtons wrong" id="wrongButton2" >With silver bullets</button>
+        <button class="allButtons wrong" id="wrongButton3">You can't</button>
+    </div>`,
+    `<p>Who killed Death?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >Dean Winchester</button>
+        <button class="allButtons wrong" id="wrongButton1">Lucifer</button>
+        <button class="allButtons wrong" id="wrongButton2" >Crowley</button>
+        <button class="allButtons wrong" id="wrongButton3">Billie</button>
+    </div>`,
+    `<p>What happened to Bella?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >She died by hellhounds</button>
+        <button class="allButtons wrong" id="wrongButton1">She lives</button>
+        <button class="allButtons wrong" id="wrongButton2" >She married and fucked off</button>
+        <button class="allButtons wrong" id="wrongButton3">She stole the colt and killed herelf with it</button>
+    </div>`,
+    `<p>Who is in heaven by the end of the show?</p>
+    <img src="images/BobbySinger.jpg" class="imagesInTheContainer">
+    <div class="allButtonsContainer">
+        <button class="allButtons right" id="correctButton" >Kevin, Dean, Sam, Death</button>
+        <button class="allButtons wrong" id="wrongButton1">Ash, Jess, Rufus, Bobby</button>
+        <button class="allButtons wrong" id="wrongButton2" >Crowley, Sam, Dean, Bobby</button>
+        <button class="allButtons wrong" id="wrongButton3">Mary, John, Sam, Azazel</button>
+    </div>`
+];
+
+
+
+     function nextQuestion() {
+        var randomNumber = Math.floor(Math.random() * (questions.length));
+        document.getElementById('containerForEverything').innerHTML =
+        questions[randomNumber];
+      };
+
+
+     
